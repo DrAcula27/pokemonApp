714ed18bd59dac7c5e376e22c8e253fb3a39664a
Danielle <danielle@kendallspace.net>
2023-01-12 17:01:57 -0500

    add ability to toggle hiding/showing image and age of pokemon

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ const route = "http://localhost:5000/get_pokemon_data";
 togglePokeListBtn.addEventListener("click", async () => {
   let res = await fetch(route);
   let pokemon = await res.json();
-  let pokemonNames = pokemon.map((el) => el.name);
+  let pokemonNames = pokemon.map((el) => `${el.name}: Choose!`);
   let body = document.querySelector("body");
   if (togglePokeListBtn.innerText.toLowerCase().includes("show")) {
     togglePokeListBtn.innerText = `Hide Pokemon List`;
@@ -19,18 +19,23 @@ togglePokeListBtn.addEventListener("click", async () => {
     const pokemonElementsArray = [...pokemonElements];
     pokemonElementsArray.forEach((element, i) => {
       element.addEventListener("click", () => {
-        element.innerHTML += `
-        <br />
-        Age: ${pokemon[i].age}
-        <br />
-        <img src="${pokemon[i].img}.jpg" />`;
+        if (element.innerHTML.toLowerCase().includes("choose")) {
+          element.innerHTML = `
+            ${pokemon[i].name}
+            <br />
+            Age: ${pokemon[i].age}
+            <br />
+            <img src="${pokemon[i].img}.jpg" />`;
+        } else {
+            element.innerHTML = `${pokemon[i].name}: Choose!`;
+        }
       });
     });
   } else {
     togglePokeListBtn.innerText = `Show Pokemon List`;
     for (let i = 0; i < pokemonNames.length; i++) {
-        let remove = document.getElementById(`pokemon${i}`);
-        body.removeChild(remove);
+      let remove = document.getElementById(`pokemon${i}`);
+      body.removeChild(remove);
     }
   }
 });
