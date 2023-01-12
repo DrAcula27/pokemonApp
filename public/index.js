7968593f3a6adcc627127d79aab9d77d36ba924d
Danielle <danielle@kendallspace.net>
2023-01-12 16:53:10 -0500

    add ability to toggle showing/hiding pokemon list

--- public/index.js
+++ public/index.js
@@ -1,28 +1,36 @@
-const showPokemonBtn = document.getElementById("show-pokemon-btn");
+const togglePokeListBtn = document.getElementById("toggle-poke-list-btn");
 const route = "http://localhost:5000/get_pokemon_data";
 
-showPokemonBtn.addEventListener("click", async () => {
+togglePokeListBtn.addEventListener("click", async () => {
   let res = await fetch(route);
   let pokemon = await res.json();
   let pokemonNames = pokemon.map((el) => el.name);
-
   let body = document.querySelector("body");
-  for (let i = 0; i < pokemonNames.length; i++) {
-    const para = document.createElement("p");
-    para.id = `pokemon${i}`;
-    para.className = "showImgAge";
-    para.textContent = `${pokemonNames[i]}`;
-    body.appendChild(para);
-  }
-  const pokemonElements = document.getElementsByClassName("showImgAge");
-  const pokemonElementsArray = [...pokemonElements];
-  pokemonElementsArray.forEach((element, i) => {
-    element.addEventListener("click", () => {
-      element.innerHTML += `
+  if (togglePokeListBtn.innerText.toLowerCase().includes("show")) {
+    togglePokeListBtn.innerText = `Hide Pokemon List`;
+    for (let i = 0; i < pokemonNames.length; i++) {
+      const para = document.createElement("p");
+      para.id = `pokemon${i}`;
+      para.className = "showImgAge";
+      para.textContent = `${pokemonNames[i]}`;
+      body.appendChild(para);
+    }
+    const pokemonElements = document.getElementsByClassName("showImgAge");
+    const pokemonElementsArray = [...pokemonElements];
+    pokemonElementsArray.forEach((element, i) => {
+      element.addEventListener("click", () => {
+        element.innerHTML += `
         <br />
         Age: ${pokemon[i].age}
         <br />
         <img src="${pokemon[i].img}.jpg" />`;
+      });
     });
-  });
+  } else {
+    togglePokeListBtn.innerText = `Show Pokemon List`;
+    for (let i = 0; i < pokemonNames.length; i++) {
+        let remove = document.getElementById(`pokemon${i}`);
+        body.removeChild(remove);
+    }
+  }
 });
