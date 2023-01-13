b8961eb2fbc772c2457eb69f1a2111f54ff2db07
Danielle <danielle@kendallspace.net>
2023-01-13 16:09:09 -0500

    add ability to remove a pokemon from the list

--- public/index.js
+++ public/index.js
@@ -4,7 +4,6 @@ const deleteElements = document.getElementById("delete-container");
 const route = "http://localhost:5000/get_pokemon_data";
 
 togglePokeListBtn.addEventListener("click", async () => {
-
   let res = await fetch(route);
   let pokemon = await res.json();
 
@@ -12,28 +11,23 @@ togglePokeListBtn.addEventListener("click", async () => {
   let list = document.querySelector(".list");
 
   if (togglePokeListBtn.innerText.toLowerCase().includes("show")) {
-
     togglePokeListBtn.innerText = `Hide Pokemon List`;
 
     for (let i = 0; i < pokemonNames.length; i++) {
-
       const listItem = document.createElement("li");
       listItem.id = `pokemon${i}`;
       listItem.className = "showImgAge";
       listItem.textContent = `${pokemonNames[i]}`;
       list.appendChild(listItem);
-
     }
-    
+
     deleteElements.classList.remove("hidden");
 
     const pokemonElements = document.getElementsByClassName("showImgAge");
     const pokemonElementsArray = [...pokemonElements];
 
     pokemonElementsArray.forEach((element, i) => {
-
       element.addEventListener("click", () => {
-
         if (element.innerHTML.toLowerCase().includes("choose")) {
           element.innerHTML = `
             ${pokemon[i].name}
@@ -41,21 +35,12 @@ togglePokeListBtn.addEventListener("click", async () => {
             Age: ${pokemon[i].age}
             <br />
             <img src="${pokemon[i].img}.jpg" />`;
-
         } else {
           element.innerHTML = `${pokemon[i].name}: Choose!`;
         }
       });
     });
-
-    deleteBtn = document.getElementById("delete-btn");
-    deleteBtn.addEventListener("click", () => {
-      const userInput = document.getElementById("delete-input");
-      
-    });
-
   } else {
-
     togglePokeListBtn.innerText = `Show Pokemon List`;
 
     deleteElements.classList.add("hidden");
@@ -64,5 +49,74 @@ togglePokeListBtn.addEventListener("click", async () => {
       let removePokemon = document.getElementById(`pokemon${i}`);
       list.removeChild(removePokemon);
     }
+  }
+});
+
+const deleteBtn = document.getElementById("delete-btn");
+
+deleteBtn.addEventListener("click", async () => {
+  let res = await fetch(route);
+  let pokemon = await res.json();
+  let list = document.querySelector(".list");
+  let userInput = document.getElementById("delete-input").value;
+
+  if (userInput.toLowerCase() === "bulbasaur") {
+    pokemon.forEach((el, i) => {
+      if (el.name === "bulbasaur") {
+        pokemon.splice(i, 1);
+      }
+    });
+    let removePokemon = document.getElementById(`pokemon0`);
+    list.removeChild(removePokemon);
+  } else if (userInput.toLowerCase() === "ivysaur") {
+    pokemon.forEach((el, i) => {
+      if (el.name === "ivysaur") {
+        pokemon.splice(i, 1);
+      }
+    });
+    let removePokemon = document.getElementById(`pokemon1`);
+    list.removeChild(removePokemon);
+  } else if (userInput.toLowerCase() === "venusaur") {
+    pokemon.forEach((el, i) => {
+      if (el.name === "venusaur") {
+        pokemon.splice(i, 1);
+      }
+    });
+    let removePokemon = document.getElementById(`pokemon2`);
+    list.removeChild(removePokemon);
+  } else if (userInput.toLowerCase() === "charmander") {
+    pokemon.forEach((el, i) => {
+      if (el.name === "charmander") {
+        pokemon.splice(i, 1);
+      }
+    });
+    let removePokemon = document.getElementById(`pokemon3`);
+    list.removeChild(removePokemon);
+  } else if (userInput.toLowerCase() === "charizard") {
+    pokemon.forEach((el, i) => {
+      if (el.name === "charizard") {
+        pokemon.splice(i, 1);
+      }
+    });
+    let removePokemon = document.getElementById(`pokemon4`);
+    list.removeChild(removePokemon);
+  } else if (userInput.toLowerCase() === "squirtle") {
+    pokemon.forEach((el, i) => {
+      if (el.name === "squirtle") {
+        pokemon.splice(i, 1);
+      }
+    });
+    let removePokemon = document.getElementById(`pokemon5`);
+    list.removeChild(removePokemon);
+  } else if (userInput.toLowerCase() === "wartortle") {
+    pokemon.forEach((el, i) => {
+      if (el.name === "wartortle") {
+        pokemon.splice(i, 1);
+      }
+    });
+    let removePokemon = document.getElementById(`pokemon6`);
+    list.removeChild(removePokemon);
+  } else {
+    console.log(`Please type the name of a pokemon in the list to delete`);
   }
 });
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 app.use(express.static("public"));
 
 // simulate data gotten from database
-const pokemon = [
+let pokemon = [
     {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur", age: 3},
     {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur", age: 4},
     {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur", age: 5},
@@ -15,6 +15,16 @@ const pokemon = [
     {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle", age: 8},
     {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle", age: 6}
 ];
+
+app.delete('/delete/:pokemonToDelete', (req, res) => {
+  // let pokemonToDelete = req.params.pokemonToDelete;
+  // pokemon.forEach((el, i)=>{
+  //     if (el.name === pokemonToDelete) {
+  //         pokemon.splice(i, 1);
+  //     }
+  // })
+  res.send(`Deleted`);
+});
 
 app.get("/get_pokemon_data", (req, res) => {
     res.send(pokemon);
